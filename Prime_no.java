85324da4dd4f152698d03c6ae9c24d28c3b75d0c
Shradha Baghel <131020003+shraddhabaghel@users.noreply.github.com>
2024-10-17 20:55:29 +0530

    Create Prime_no.java

---
+++ Prime_no.java
@@ -0,0 +1,27 @@
+// Java Program to demonstrate Brute Force Method to check if a number is prime
+class GFG {
+    static boolean isPrime(int n)
+    {
+
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+
+    public static void main(String args[])
+    {
+        if (isPrime(11))
+            System.out.println(" true");
+        else
+            System.out.println(" false");
+        if (isPrime(15))
+            System.out.println(" true");
+        else
+            System.out.println(" false");
+    }
+}
